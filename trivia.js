fca9ddcb9c89845a225b17b0f9829d07d2f9f341
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-09-06 20:56:50 -0700

    update game

--- trivia.js
+++ trivia.js
@@ -1,61 +1,69 @@
- var triviaQuestions = {
+ var triviaQuestions = [{
  	question: "What is you favorite color?", answers: {
  		a: "blue",
  		b: "green",
  		c: "red",
  	},
  	correctAnswer: "a",
+ },
 
- 	question: "What is your favorite food?", answers: {
+ 	{question: "What is your favorite food?", answers: {
  		a: "Tacos",
  		b: "pie",
  		c: "pizza",
  	},
- 	correctAnswer: "Tacos",
- };
- var quesionTimer = 0;
+ 	correctAnswer: "a",
+ },
+
+ ];
+
+
+
+
+
+
+var generateQuiz =function (){
+
  var quiz = document.getElementById("quiz");
  var results = document.getElementById("results");
  var submit = document.getElementById("submit");
+ var questionTimer = 0;
 
-
-
-
-var generateQuiz =function (triviaQuestions, buildQuiz, showResults, submit){
-
-	var buildQuiz = function (triviaQuestions, quiz){
+	var buildQuiz = function (){
 		var output = [];
       	var answers = [];
 
       	for (var i = 0; i < triviaQuestions.length; i++) {
+      		console.log(i);
       		answers = [];
       		for (letter in triviaQuestions[i].answers) {
       			answers.push("<label>"
-      			+ '<input type="radio" name="question +i" + value="+letter+">'
+      			+ '<input type="radio" name=triviaQuestion' + i + ' value="'+letter+'">'
 					+ letter + ": "
-					+ questions[i].answers[letter]+ "</label>")
+					+ triviaQuestions[i].answers[letter]+ "</label>")
       		};
 
       		output.push(
 			'<div class="question">' + triviaQuestions[i].question + '</div>'
-			+ '<div class="answers">' + answers.join('') + '</div>')
+			+ '<div class="answers">' + answers.join(' ') + '</div>')
 
 			quiz.innerHTML = output.join('');
+			console.log(output.join(''));
 
      };
 };
      
-	var showResults = function (triviaQuestions, quiz, results){
+	var showResults = function (){
 
 		var answerDisplay = quiz.querySelectorAll('.answers');
 		var userAnswer = '';
 		var numCorrect = 0;
 
-		for(var i=0; i<questions.length; i++){
+		for(var i=0; i<triviaQuestions.length; i++){
 
-			userAnswer = (answerDisplay[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-			
-			if(userAnswer===questions[i].correctAnswer){
+			userAnswer = (answerDisplay[i].querySelector('input[name=triviaQuestion'+i+']:checked')||{}).value;
+			console.log("User answer: " + userAnswer);
+			if(userAnswer === triviaQuestions[i].correctAnswer){
 				numCorrect++;
 				
 				answerDisplay[i].style.color = "lightgreen";
@@ -65,15 +73,19 @@ var generateQuiz =function (triviaQuestions, buildQuiz, showResults, submit){
 			}
 		}
 
-		results.innerHTML = numCorrect + ' out of ' + questions.length;
+		results.innerHTML = numCorrect + ' out of ' + triviaQuestions.length;
 	};
+
+	buildQuiz();
+	console.log("build quiz done");
 
 	$("#submit").on("click", showResults);
 };
 
 $(document).ready(function() {
 
-generateQuiz(triviaQuestions, buildQuiz, showResults, submit);
+generateQuiz();
+console.log("generate quiz done")
 
 });
 
